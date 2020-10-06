cb797bae93872342c0de18974f822f4186f21f82
Yodasz <72433417+Yodasz@users.noreply.github.com>
2020-10-06 10:01:04 +0200

    anoder niu apdejt

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+
+function info(pkt, inform) {
+    var punkt = document.getElementById(pkt);
+    var information = document.getElementById(inform);
+
+    punkt.addEventListener('mouseenter', function () {
+        information.classList.remove('hide');
+        information.classList.add('show');
+    })
+    punkt.addEventListener('mouseleave', function () {
+        information.classList.remove('show');
+        information.classList.add('hide');
+    })
+}
+
+info('cpu','cpuInfo');
+info('ram','ramInfo');
+info('pcie','pcieInfo');
+info( 'sata','sataInfo');
+info('gniazda','gniazdaInfo');
